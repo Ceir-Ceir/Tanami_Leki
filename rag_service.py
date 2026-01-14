b3716a94fd765ebe6ab77a7c9e57dbd12c1a57e9
Eric Green <playboi@Erics-MacBook-Pro.local>
2026-01-13 23:10:53 -0500

    added port 10000

--- rag_service.py
+++ rag_service.py
@@ -201,5 +201,5 @@ def health():
 
 if __name__ == '__main__':
     # Run on port 5000 (default) or PORT env var
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
