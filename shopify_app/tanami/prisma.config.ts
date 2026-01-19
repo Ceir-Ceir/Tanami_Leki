37ca67b96452fd069b4fdae3f70aba96cc1f2917
Eric Green <playboi@Erics-MacBook-Pro.local>
2026-01-19 18:40:59 -0500

    removed prisma config somthing

--- shopify_app/tanami/prisma.config.ts
+++ shopify_app/tanami/prisma.config.ts
@@ -4,8 +4,4 @@ import { defineConfig, env } from '@prisma/config';
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
-  datasource: {
-    // This will now successfully find DIRECT_URL from your .env
-    url: env('DIRECT_URL'),
-  },
 });
